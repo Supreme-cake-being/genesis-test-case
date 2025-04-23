162f2f05449c9660ec1d94e2a78b8bb39a46a164
Karr4 but from another universe <XGon20987@gmail.com>
2025-04-23 18:02:43 +0300

    changed the name of the useFetch hook

---
+++ frontend/src/hooks/useFetch.ts
@@ -0,0 +1,58 @@
+import axios from "axios";
+import { useState, useEffect, useCallback } from "react";
+import { ISearchParams } from "@/src/types/searchParams";
+
+export const useFetch = (endpoint = "") => {
+  const [data, setData] = useState<Record<string, any> | null>(null);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState<Record<string, any> | null>(null);
+
+  const handleFetch = useCallback(async () => {
+    try {
+      const response = await axios.get(`/${endpoint}`);
+      setData(response);
+    } catch (error) {
+      setError(error as Record<string, any>);
+    } finally {
+      setLoading(false);
+    }
+  }, []);
+
+  useEffect(() => {
+    setData(null);
+    setLoading(true);
+    setError(null);
+    handleFetch();
+  }, [endpoint]);
+
+  return { data: data?.data, loading, error };
+};
+
+export const useLazyFetch = (endpoint = "") => {
+  const [data, setData] = useState<Record<string, any> | null>(null);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState<Record<string, any> | null>(null);
+
+  const handleFetch = async ({
+    page,
+    limit,
+    sort,
+    order,
+    search,
+    genre,
+    artist,
+  }: ISearchParams) => {
+    try {
+      const response = await axios.get(`/${endpoint}`, {
+        params: { page, limit, sort, order, search, genre, artist },
+      });
+      setData(response);
+    } catch (error) {
+      setError(error as Record<string, any>);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  return { data: data?.data, loading, error, handleFetch };
+};

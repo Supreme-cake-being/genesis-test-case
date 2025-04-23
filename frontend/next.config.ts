199a90afd137607da633ebcb92d2d38f6503ff81
Karr4 but from another universe <XGon20987@gmail.com>
2025-04-23 18:00:07 +0300

    added redirect to tracks page from root route

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -3,6 +3,17 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: true,
+
+  async redirects() {
+    return [
+      // Basic redirect
+      {
+        source: "/",
+        destination: "/tracks",
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;

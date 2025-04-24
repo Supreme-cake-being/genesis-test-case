2f7012a261293f387c58e91a197fb8aedd81e615
Karr4 but from another universe <XGon20987@gmail.com>
2025-04-24 21:10:08 +0300

    renamed hook

--- frontend/src/hooks/useTrackBySlug.ts
+++ frontend/src/hooks/useTrackBySlug.ts
@@ -1,6 +1,6 @@
 import { useFetch } from "./useFetch";
 
-export const useTrackById = (slug: string) => {
+export const useTrackBySlug = (slug: string) => {
   const { data, loading, error } = useFetch(`tracks/${slug}`);
 
   return { track: data, loading, error };
